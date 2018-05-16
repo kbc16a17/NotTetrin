a4f9406b16b955d6dfc21935745b391be83774ce
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-05-17 01:42:30 +0900

    --allow-empty

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,41 +6,42 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
-    public GameObject MinoSpawner;
-    [SerializeField]
-    private Score score;
+    public NextMino NextMino;
+    public MinoSpawner MinoSpawner;
+    public Score Score;
 
     public UnityEvent OnRoundStart;
     public UnityEvent OnRoundEnd;
 
 	// Use this for initialization
 	void Start () {
-        NextRound();
+        nextRound();
 	}
 	
-    private void NextMino() {
-        var obj = MinoSpawner.GetComponent<MinoSpawner>().Spawn();
+    private void nextMino() {
+        var obj = MinoSpawner.Spawn();
+        NextMino.UpdateMinos();
         var controller = obj.GetComponent<MinoController>();
-        controller.Hit += OnMinoHit;
-        controller.HitOnCeiling += OnMinoHitOnCeiling;
+        controller.Hit += onMinoHit;
+        controller.HitOnCeiling += onMinoHitOnCeiling;
     }
 
-    private void NextRound() {
+    private void nextRound() {
         OnRoundStart.Invoke();
 
-        score.Reset();
-        NextMino();
+        Score.Reset();
+        nextMino();
     }
 
-    private void OnMinoHit(object sender, EventArgs args) {
+    private void onMinoHit(object sender, EventArgs args) {
         var controller = sender as MinoController;
-        controller.Hit -= OnMinoHit;
-        score.Increase(100);
-        NextMino();
+        controller.Hit -= onMinoHit;
+        Score.Increase(100);
+        nextMino();
     }
 
-    private void OnMinoHitOnCeiling(object sender, EventArgs args) {
+    private void onMinoHitOnCeiling(object sender, EventArgs args) {
         OnRoundEnd.Invoke();
-        Invoke("NextRound", 3.0f);
+        Invoke("nextRound", 3.0f);
     }
 }
---
+++ Assets/Scripts/NextMino.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextMino : MonoBehaviour {
+    public MinoSpawner spawner;
+    public MinoResolver resolver;
+    public Transform[] frames;
+
+    private List<GameObject> objects = new List<GameObject>();
+
+	public void UpdateMinos () {
+        foreach (var obj in objects) {
+            Destroy(obj);
+        }
+        objects.Clear();
+
+        for (int i = 0; i < frames.Length; i++) {
+            var index = spawner.NextIndices[i];
+            var obj = Instantiate(resolver.Get(index), frames[i].position, Quaternion.identity);
+            obj.GetComponent<MinoController>().enabled = false;
+            obj.GetComponent<Rigidbody2D>().simulated = false;
+            objects.Add(obj);
+        }
+    }
+}
